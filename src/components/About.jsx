d876225dec8abcb7efbacf87b29a8a6cedffa708
Himanshu Negi <himanshu.21negi@gmail.com>
2023-06-11 23:53:36 +0530

    About section improved

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import SectionWrapper from "../hoc/SectionWrapper";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -52,4 +53,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
